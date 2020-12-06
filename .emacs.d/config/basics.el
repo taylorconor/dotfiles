490c7c434b0066a3f2d5f0b59faae67b410df661
Conor Taylor <taylorconor@google.com>
2020-12-07 00:09:36 +0100

    Merge some emacs config files

--- .emacs.d/config/basics.el
+++ .emacs.d/config/basics.el
@@ -1,3 +1,7 @@
+;; configure backups
 (setq backup-directory-alist `(("." . "~/.emacs_backups/")))
 (setq backup-by-copying t)
 (setq delete-old-versions t)
+
+;; follow symlinks
+(setq vc-follow-symlinks t)
--- .emacs.d/config/indentation.el
+++
@@ -1,4 +0,0 @@
-;; stop emacs from indenting inside a namespace scope
-(defun my-c-setup ()
-   (c-set-offset 'innamespace [0]))
-(add-hook 'c++-mode-hook 'my-c-setup)

721111425738b18bf9b7f9e665a0660d869f89ba
Conor Taylor <conor@taylor.sh>
2021-02-21 22:52:51 +0100

    Add ido-mode to basics

--- .emacs.d/config/basics.el
+++ .emacs.d/config/basics.el
@@ -5,3 +5,11 @@
 
 ;; follow symlinks
 (setq vc-follow-symlinks t)
+
+;; ido mode is fundamental
+(require 'ido-vertical-mode)
+(ido-mode 1)
+(ido-vertical-mode 1)
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(setq ido-vertical-define-keys 'C-n-and-C-p-only)
